d48bbc4017ef505275619f21d81874a5a067bf52
tomotins <tom.doran5454@gmail.com>
2025-10-13 16:32:41 +0200

    I have removed one test function, as it didnt work right, and I have changed the other, because it was still treating the cosine, as a sin, obviously making the test fail, not it should pass

--- test.py
+++ test.py
@@ -11,18 +11,11 @@ def test_1():
 
 test_1()
 
+
 def test_2():
-    sin_signal = np.sin(t)
-    assert len(sin_signal) == 2000, "Length of time array should be 2000 for 2 seconds at 1000 Hz sampling rate"
-    assert np.isclose(sin_signal[0], 0), "First value of sine wave should be 0"
-    assert np.isclose(sin_signal[500], 0)
-
-test_2()
-
-def test_3():
     t_cos, signal_cos = cosine_wave(freq=5, duration=2)
     assert len(t_cos) == 2000, "Length of time array should be 2000 for 2 seconds at 1000 Hz sampling rate"
-    assert np.isclose(signal_cos[0], 0), "First value of sine wave should be 0"
-    assert np.isclose(signal_cos[500], 0)
+    assert np.isclose(signal_cos[0], 1), "first value of cosine wave should be 1"
+    assert np.isclose(signal_cos[500], 0) " value at t=0.5s should be 0"
 
-test_3()
+test_2()
